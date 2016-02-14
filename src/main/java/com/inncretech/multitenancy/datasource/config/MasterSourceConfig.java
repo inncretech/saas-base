0bb8ac7ed825f9fd5a3e1f070f41a639b5de4b13
deepaks <deepaks@ebay.com>
2016-02-13 18:23:20 -0800

    Restructed the code and added test case.

--- src/main/java/com/inncretech/multitenancy/datasource/config/MasterSourceConfig.java
+++ src/main/java/com/inncretech/multitenancy/datasource/config/MasterSourceConfig.java
@@ -13,61 +13,61 @@ import com.inncretech.multitenancy.datasource.utils.DataSourceUtils;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
-@EnableJpaRepositories(basePackages = { "com.vxlpartners.appanalytix.master.db.repository" }, entityManagerFactoryRef = "masterEntityManagerFactory", transactionManagerRef = "masterTransactinManager")
+@EnableJpaRepositories(basePackages = {
+        "com.inncretech.multitenancy.datasource.master.dao" }, entityManagerFactoryRef = "masterEntityManagerFactory", transactionManagerRef = "masterTransactinManager")
 public class MasterSourceConfig {
 
-	private String driverClass = "com.mysql.jdbc.Driver";
+    private String driverClass = "com.mysql.jdbc.Driver";
 
-	@Value(value = "${master.config.initialPoolSize}")
-	private int initialPoolSize = 3;
+    @Value(value = "${master.config.initialPoolSize}")
+    private int initialPoolSize = 3;
 
-	@Value(value = "${master.config.minPoolSize}")
-	private int minPoolSize;
+    @Value(value = "${master.config.minPoolSize}")
+    private int minPoolSize;
 
-	@Value(value = "${master.config.maxPoolSize}")
-	private int maxPoolSize;
+    @Value(value = "${master.config.maxPoolSize}")
+    private int maxPoolSize;
 
-	@Value(value = "${master.config.acquireIncrement}")
-	private int acquireIncrement;
+    @Value(value = "${master.config.acquireIncrement}")
+    private int acquireIncrement;
 
-	@Value(value = "${master.config.jdbcUrl}")
-	private String jdbcUrl;
+    @Value(value = "${master.config.jdbcUrl}")
+    private String jdbcUrl;
 
-	@Value(value = "${master.config.user}")
-	private String user;
+    @Value(value = "${master.config.user}")
+    private String user;
 
-	@Value(value = "${master.config.password}")
-	private String password;
+    @Value(value = "${master.config.password}")
+    private String password;
 
-	@Bean(name = "masterDataSource")
-	public ComboPooledDataSource masterDataSource() throws Exception {
-		ComboPooledDataSource comboPooledDataSource = DataSourceUtils.createDataSource(driverClass, initialPoolSize,
-				minPoolSize, maxPoolSize, acquireIncrement, jdbcUrl, user, password);
-		return comboPooledDataSource;
+    @Bean(name = "masterDataSource")
+    public ComboPooledDataSource masterDataSource() throws Exception {
+        ComboPooledDataSource comboPooledDataSource = DataSourceUtils.createDataSource(driverClass, initialPoolSize, minPoolSize, maxPoolSize,
+                acquireIncrement, jdbcUrl, user, password);
+        return comboPooledDataSource;
 
-	}
+    }
 
-	@Bean(name = "masterEntityManagerFactory")
-	public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-			@Qualifier("masterDataSource") ComboPooledDataSource masterDataSource) throws Exception {
-		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
-		localContainerEntityManagerFactoryBean.setDataSource(masterDataSource);
-		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-		jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
-		jpaVendorAdapter.setGenerateDdl(true);
-		jpaVendorAdapter.setShowSql(true);
-		localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-		localContainerEntityManagerFactoryBean.setPersistenceUnitName("masterPersistentUnit");
-		localContainerEntityManagerFactoryBean.setPackagesToScan("com.vxlpartners.appanalytix.master.db.entity");
-		return localContainerEntityManagerFactoryBean;
-	}
+    @Bean(name = "masterEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(@Qualifier("masterDataSource") ComboPooledDataSource masterDataSource)
+            throws Exception {
+        LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        localContainerEntityManagerFactoryBean.setDataSource(masterDataSource);
+        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL5Dialect");
+        jpaVendorAdapter.setGenerateDdl(true);
+        jpaVendorAdapter.setShowSql(true);
+        localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        localContainerEntityManagerFactoryBean.setPersistenceUnitName("masterPersistentUnit");
+        localContainerEntityManagerFactoryBean.setPackagesToScan("com.inncretech.multitenancy.datasource.master.entity");
+        return localContainerEntityManagerFactoryBean;
+    }
 
-	@Bean(name = "masterTransactinManager")
-	public JpaTransactionManager transactionManager(
-			@Qualifier("masterEntityManagerFactory") LocalContainerEntityManagerFactoryBean masterEntityManagerFactory)
-			throws Exception {
-		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
-		jpaTransactionManager.setEntityManagerFactory(masterEntityManagerFactory.getObject());
-		return jpaTransactionManager;
-	}
+    @Bean(name = "masterTransactinManager")
+    public JpaTransactionManager transactionManager(
+            @Qualifier("masterEntityManagerFactory") LocalContainerEntityManagerFactoryBean masterEntityManagerFactory) throws Exception {
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+        jpaTransactionManager.setEntityManagerFactory(masterEntityManagerFactory.getObject());
+        return jpaTransactionManager;
+    }
 }
---
+++ src/test/java/com/inncretech/multitenancy/datasource/config/TestMultiTenancyConfiguration.java
@@ -0,0 +1,52 @@
+package com.inncretech.multitenancy.datasource.config;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import com.inncretech.multitenancy.datasource.config.MultiTenancyConfiguration;
+import com.inncretech.multitenancy.datasource.config.RoutingDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { MultiTenancyConfiguration.class })
+@ActiveProfiles("dev")
+public class TestMultiTenancyConfiguration {
+
+    @Autowired
+    @Qualifier("masterDataSource")
+    private ComboPooledDataSource masterDataSource;
+
+    @Autowired
+    @Qualifier("routingDataSource")
+    private RoutingDataSource routingDatasource;
+
+    @Test
+    public void masterDataSource() {
+        Assert.notNull(masterDataSource);
+    }
+
+    @Test
+    public void routingDataSource() throws SQLException {
+        Assert.notNull(routingDatasource);
+        DbContextHolder.setDbType(1L);
+        Connection connection = routingDatasource.getConnection();
+        Assert.state(connection.getCatalog().equals("db1"));
+        Assert.notNull(routingDatasource);
+        DbContextHolder.setDbType(2L);
+        connection = routingDatasource.getConnection();
+        Assert.state(connection.getCatalog().equals("db2"));
+        Assert.notNull(routingDatasource);
+        DbContextHolder.setDbType(3L);
+        connection = routingDatasource.getConnection();
+        Assert.state(connection.getCatalog().equals("db3"));
+    }
+}
