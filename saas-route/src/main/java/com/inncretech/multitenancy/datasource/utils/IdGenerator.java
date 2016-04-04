c6e79992c2e02fa7d9e20d0b4d6d50c085e171a0
rmmaheshwari <radhamohanmaheshwari@gmail.com>
2016-04-04 22:25:36 +0530

    id generator changes

--- saas-route/src/main/java/com/inncretech/multitenancy/datasource/utils/IdGenerator.java
+++ saas-route/src/main/java/com/inncretech/multitenancy/datasource/utils/IdGenerator.java
@@ -1,45 +1,63 @@
 package com.inncretech.multitenancy.datasource.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.io.Serializable;
+
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SessionImplementor;
+import org.hibernate.id.IdentifierGenerator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import com.inncretech.multitenancy.datasource.dto.TenantAware;
 import com.inncretech.multitenancy.datasource.master.dao.IdEntryRepository;
 import com.inncretech.multitenancy.datasource.master.entity.IdEntry;
 
 @Component
-public class IdGenerator {
+public class IdGenerator implements IdentifierGenerator, ApplicationContextAware {
 
-    private static final int TIME_BITS = 41;
+	private static ApplicationContext applicationContext;
 
-    private static final int SHARD_BITS = 13;
+	private static final int TIME_BITS = 41;
 
-    private static final int TOTAL_BITS = 64;
+	private static final int SHARD_BITS = 13;
 
-    private static final int SHARD_ID = 1;
+	private static final int TOTAL_BITS = 64;
 
-    @Autowired
-    private IdEntryRepository idEntryRepository;
+	@SuppressWarnings("static-access")
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
 
-    public Long create(int shardId) {
-        Long id = System.currentTimeMillis() << (64 - TIME_BITS);
-        id |= shardId << (TOTAL_BITS - TIME_BITS - SHARD_BITS);
-        id |= (nextSequence() % (int) Math.pow(2.0, (double) (TOTAL_BITS - SHARD_BITS - TIME_BITS)));
-        id &= 0X3FFFFFFFFFFFFFFFL;
-        return id;
-    }
+	public Long create(int shardId) {
+		Long id = System.currentTimeMillis() << (64 - TIME_BITS);
+		id |= shardId << (TOTAL_BITS - TIME_BITS - SHARD_BITS);
+		id |= (nextSequence() % (int) Math.pow(2.0, (double) (TOTAL_BITS - SHARD_BITS - TIME_BITS)));
+		id &= 0X3FFFFFFFFFFFFFFFL;
+		return id;
+	}
 
-    public Long create() {
-        return create(SHARD_ID);
-    }
+	private static final int SHARD_ID = 1;
 
-    private Long nextSequence() {
-        return idEntryRepository.save(new IdEntry()).getId();
-    }
+	public Long create() {
+		return create(SHARD_ID);
+	}
 
-    public Integer getShardId(Long id) {
-        Long newId = new Long(id);
-        newId = newId >> 10;
-        newId = newId & 0X3FFL;
-        return newId.intValue();
-    }
+	private Long nextSequence() {
+		return applicationContext.getBean(IdEntryRepository.class).save(new IdEntry()).getId();
+	}
+
+	public Integer getShardId(Long id) {
+		Long newId = new Long(id);
+		newId = newId >> 10;
+		newId = newId & 0X3FFL;
+		return newId.intValue();
+	}
+
+	@Override
+	public Serializable generate(SessionImplementor session, Object object) throws HibernateException {
+		TenantAware tenantAware = (TenantAware) object;
+		return create(tenantAware.getTenantId().intValue());
+	}
 }
