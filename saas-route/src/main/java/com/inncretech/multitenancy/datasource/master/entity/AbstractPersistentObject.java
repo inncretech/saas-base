4ed61de87202897b7834869eaa293aaee1b8994d
“deepaks” <deepaks@gmail.com>
2016-04-23 17:45:38 -0700

    Added name and description

--- saas-route/src/main/java/com/inncretech/multitenancy/datasource/master/entity/AbstractPersistentObject.java
+++ saas-route/src/main/java/com/inncretech/multitenancy/datasource/master/entity/AbstractPersistentObject.java
@@ -1,5 +1,6 @@
 package com.inncretech.multitenancy.datasource.master.entity;
 
+import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Version;
 
@@ -8,6 +9,13 @@ public abstract class AbstractPersistentObject extends AuditData {
 
     @Version
     private Integer version;
+    
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
 
     public abstract Long getId();
 
@@ -19,5 +27,29 @@ public abstract class AbstractPersistentObject extends AuditData {
 
     public void setVersion(Integer version) {
         this.version = version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("AbstractPersistentObject [version=").append(version).append(", name=").append(name).append(", description=")
+                .append(description).append("]");
+        return builder.toString();
     }
 }
